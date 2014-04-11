44d14081d96efb803dcdcba2d538f2a785a807b8
Roooommmmelllll <wassahman@gmail.com>
2014-04-10 20:00:25 -0700

    added number 3 to the midterm_1 file

--- midterm_1/midterm_1.py
+++ midterm_1/midterm_1.py
@@ -12,7 +12,57 @@
 
 #Think about whether you should read the data in line by line.
 
-def main():
+def checkLength(line):
+   if len(line) > 8:
+      return 1
+   else:
+      return 0
+
+def checkZ(line):
+   if line[0] == "z" or line[0] == "Z":
+      return 1
+   else:
+      return 0
+
+def checkPal(line):
+   line = line[0:len(line)-1]
+   reverse = line[::-1]
+   if str(reverse) == str(line):
+      return True
+   else:
+      return False
+   
+def problem2():
+   readFile = open("allwords.txt", "r")
+   outFile = open("results3.txt", "w")
+   longWords = 0
+   numberZs = 0
+   palList = []
+   
+   for line in readFile:
+      longWords = longWords + checkLength(line)
+      numberZs = numberZs + checkZ(line)
+      if checkPal(line) == True:
+         palindrome = line[0:len(line)-1]
+         palList.append(palindrome)
+      
+   toWrite = "Rommel Niduaza\nApril 10, 2014\n\n"
+   toWrite += "Number of words longer than 8 letters: "
+   toWrite += str(longWords)+"\n\n"
+   toWrite += "Number of words starting with the letter 'Z': "
+   toWrite += str(numberZs)+"\n\n"
+   toWrite += "List of words that are palindromes:\n"
+
+   outFile.write(toWrite)
+   
+   print(palList)
+
+   for i in range(len(palList)):
+      outFile.write(str(palList[i])+"\n")
+   outFile.close()
+   readFile.close()
+
+def problem1():
    studentFile = open("studentdata.txt", "r")
    resultFile = open("results1.txt", "w")
 
@@ -39,5 +89,8 @@ def main():
 
    resultFile.close()
    studentFile.close()
+ 
+def main():
+   problem2()
 
 main()
